3a614d3fa21e0650cd08e1f2048f6ccd55094747
Luke <l.pistrol@gmail.com>
2021-12-07 17:51:04 +0100

    Initial Commit

---
+++ Shared/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  Shared
+//
+//  Created by Lukas Pistrol on 07.12.21.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
