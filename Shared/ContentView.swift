8545dc20eb2078e122deb86be0b293754153e8ce
Luke <l.pistrol@gmail.com>
2021-12-08 02:08:31 +0100

    initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	
+	@EnvironmentObject var lifeModel: LifeModel
+	
+	@State private var timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
+	
+	var body: some View {
+		VStack(spacing: 2) {
+			let items: Array<GridItem> = .init(repeating: item, count: lifeModel.grid.columns)
+			
+			LazyVGrid(columns: items, spacing: 1) {
+				ForEach(lifeModel.cells, id: \.id) { cell in
+					CellView(cell: cell)
+				}
+			}
+			.background(Color.black)
+			.padding()
+			HStack {
+				
+				Button(action: {
+					self.timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
+				}, label: {
+					Image(systemName: "play.fill")
+				})
+					.help("run")
+				
+				Button(action: {
+					self.timer.upstream.connect().cancel()
+				}, label: {
+					Image(systemName: "pause.fill")
+				})
+					.help("stop")
+				
+				Button(action: {
+					self.lifeModel.lifeCycle()
+				}, label: {
+					Image(systemName: "forward.end.fill")
+				})
+					.help("step_forward")
+				
+				Button(action: {
+					self.lifeModel.reset()
+				}, label: {
+					Image(systemName: "arrow.clockwise")
+				})
+					.help("reset")
+				
+			}
+			.controlSize(.large)
+			.padding()
+		}
+		.onReceive(timer) { _ in
+			//Task(priority: .high) {
+			DispatchQueue.main.async {
+				lifeModel.lifeCycle()
+			}
+		}
+	}
+	
+	var item: GridItem {
+		var item: GridItem = .init()
+		item.spacing = 1
+		return item
+	}
+}
+
+struct CellView: View {
+	
+	var cell: Cell
+	
+	var body: some View {
+		Rectangle()
+			.foregroundColor(cell.isAlive! ? .green : .clear)
+			.aspectRatio(1, contentMode: .fit)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+			.environmentObject(LifeModel())
+	}
 }
--- Shared/GameOfLifeApp.swift
+++ Shared/GameOfLifeApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct GameOfLifeApp: App {
+	
+	@StateObject private var lifeModel: LifeModel = .init(with: 60)
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
+				.environmentObject(self.lifeModel)
+			#if os(macOS)
+				.frame(minWidth: 500, idealWidth: 600, maxWidth: .infinity, minHeight: 600, idealHeight: 700, maxHeight: .infinity)
+			#endif
         }
     }
 }
